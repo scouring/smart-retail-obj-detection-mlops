fd115898f017defcc25d23857df17d5b0568937c
Sherry Courington <scouring@bu.edu>
2026-01-03 12:06:28 -0600

    add modified evaluate.py to get confusion matrix from container

--- training/evaluate.py
+++ training/evaluate.py
@@ -2,12 +2,17 @@ import os
 import json
 import boto3
 from ultralytics import YOLO
+import datetime
 
 BUCKET = os.environ["S3_BUCKET"]
 
+# Load the model
 model = YOLO("runs/detect/train/weights/best.pt")
+
+# Run validation
 metrics = model.val()
 
+# Save metrics to S3
 metrics_dict = metrics.results_dict
 
 with open("metrics.json", "w") as f:
@@ -16,5 +21,14 @@ with open("metrics.json", "w") as f:
 s3 = boto3.client("s3")
 s3.upload_file("metrics.json", BUCKET, "metrics/latest/metrics.json")
 
-print("Metrics uploaded to S3")
+# Save metrics to S3
+timestamp = datetime.now(datetime.timezone.utc).strftime("%Y%m%d-%H%M%S")
+
+# Upload all files in runs/detect/val/
+val_dir = "runs/detect/val"
+for file in os.listdir(val_dir):
+    local_path = os.path.join(val_dir, file)
+    s3.upload_file(local_path, BUCKET, f"metrics/{timestamp}/{file}")
+
+print("Metrics uploaded to S3:", timestamp)
 
